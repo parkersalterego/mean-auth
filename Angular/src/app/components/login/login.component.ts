3337259eae7a96092353f28e7465a5870c1cf9c8
parkersalterego <parkersalterego92@gmail.com>
2018-01-08 19:48:00 -0800

    angular login written and pushing json web token to local storage and user data

--- Angular/src/app/components/login/login.component.ts
+++ Angular/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: string;
 
-  constructor() { }
+  constructor(
+              private authService: AuthService,
+              private router: Router,
+              private flashMessageService: FlashMessagesService
+
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.authenticateUser(user).subscribe( data => {
+      if (!data.success) {
+        this.flashMessageService.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
+        this.router.navigate(['/login']);
+      } else {
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessageService.show(data.msg, {cssClass: 'alert-success', timeout: 5000});
+        this.router.navigate(['/dashboard']);
+      }
+    });
   }
 
 }
